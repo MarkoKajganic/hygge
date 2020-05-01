13c4c4d26734758c4b0eb5b49ad18d35c55cbcd3
KajganicMarko <kajganicmarko@gmail.com>
2020-05-01 17:17:11 +0200

    routing and blank components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [
-	
+	{ path: 'login', component: LoginComponent },
+	{ path: 'news', component: NewsComponent },
+	{ path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
